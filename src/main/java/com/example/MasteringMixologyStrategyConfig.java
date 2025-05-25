bde22b497f9d3e276feb35653fa0e26f135238ea
Saladin Afoh <saladuit@gmail.com>
2025-05-25 11:42:33 +0200

    Added strategy configuration

--- src/main/java/com/example/MasteringMixologyStrategyConfig.java
+++ src/main/java/com/example/MasteringMixologyStrategyConfig.java
@@ -4,16 +4,39 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+import java.util.Comparator;
+
+@ConfigGroup("masteringmixology")
 public interface MasteringMixologyStrategyConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+			keyName = "strategy",
+			name = "Select Strategy",
+			description = "Choose the strategy to simulate during mixology"
 	)
-	default String greeting()
+	default StrategyType strategy()
 	{
-		return "Hello";
+		return StrategyType.FULL_ORDERS;
+	}
+
+	enum StrategyType
+	{
+		FULL_ORDERS("Full orders"),
+		SKIP_TRIPLES_UNLESS_MAL("Reduce triples"),
+		REDUCE_DOUBLE_AGA("Reduce double aga"),
+		FULL_ORDER_IF_LYE_4PLUS("Full order if lye 4+"),
+		OPTIMISED_POINT_DISTRUBUTION("Optimised point distribution");
+
+		private final String name;
+
+		StrategyType(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
 	}
 }
+
